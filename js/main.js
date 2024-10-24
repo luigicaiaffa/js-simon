5fb0d00a2e0e6eced92738e3d6b3786e28ba2042
luigicaiaffa <luigicaiaffa8@gmail.com>
2024-10-24 15:33:55 +0200

    stamp numbers

--- js/main.js
+++ js/main.js
@@ -3,16 +3,36 @@
 // # RACCOLTA DATI
 
 // recupero elementi pagina
-const inputNumberEl1 = document.getElementById("input-num-1")
-const inputNumberEl2 = document.getElementById("input-num-2")
-const inputNumberEl3 = document.getElementById("input-num-3")
-const inputNumberEl4 = document.getElementById("input-num-4")
-const inputNumberEl5 = document.getElementById("input-num-5")
+const baseRandomNums = document.getElementById("random-nums");
+
+
+// preparo variabile array numeri generati
+let numbersGenerated = [];
+// parametro max 
+const numMax = 99;
 
 // # PROGRAMMA
 
-// funzione che genera numeri casuali
-const generateRandomNumbers = (max) => {
+// funzione che genera un numero casuale
+function generateRandomNumbers(max) {
   return Math.ceil(Math.random() * max);
+}
+
+// funzione che stampa in pagina 5 numeri casuali
+const stampRandomNums = () => {
+  for (let i = 0; i < 5; i++) {
+    // genero 5 numeri random
+    const genNum = generateRandomNumbers(numMax);
+    // inserisco numeri generati nell'array
+    numbersGenerated.push(genNum);
+  }
+
+  // stampo in pagina i numeri
+  baseRandomNums.innerText = numbersGenerated.join(" || ");
 };
 
+stampRandomNums()
+
+
+// | DEBUG CONSOLE LOG
+console.log(numbersGenerated);
