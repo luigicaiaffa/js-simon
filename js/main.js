272972bb060325e7cc6f010fec8a883ee062236e
luigicaiaffa <luigicaiaffa8@gmail.com>
2024-10-24 14:16:45 +0200

    random num generator

--- js/main.js
+++ js/main.js
@@ -1,1 +1,18 @@
+// ! ELABORAZIONE
+
+// # RACCOLTA DATI
+
+// recupero elementi pagina
+const inputNumberEl1 = document.getElementById("input-num-1")
+const inputNumberEl2 = document.getElementById("input-num-2")
+const inputNumberEl3 = document.getElementById("input-num-3")
+const inputNumberEl4 = document.getElementById("input-num-4")
+const inputNumberEl5 = document.getElementById("input-num-5")
+
+// # PROGRAMMA
+
+// funzione che genera numeri casuali
+const generateRandomNumbers = (max) => {
+  return Math.ceil(Math.random() * max);
+};
 
