ceb97a65c604941f7435b2837307566950dd4f13
luigicaiaffa <luigicaiaffa8@gmail.com>
2024-10-24 16:24:54 +0200

    raccolta numeri inseriti

--- js/main.js
+++ js/main.js
@@ -3,18 +3,22 @@
 // # RACCOLTA DATI
 
 // recupero elementi pagina
-const gameRandomNums = document.getElementById("random-nums");
-const gameInput = document.getElementById("game-input-user");
+const gameRandomNumsBox = document.getElementById("random-nums");
+const gameInputBox = document.getElementById("game-input-box");
+const btnGameResult = document.getElementById("btn-end-game");
 
 // elementi input box
-const inputBoxEl1 = document.getElementById("input-box-1");
-const inputBoxEl2 = document.getElementById("input-box-2");
-const inputBoxEl3 = document.getElementById("input-box-3");
-const inputBoxEl4 = document.getElementById("input-box-4");
-const inputBoxEl5 = document.getElementById("input-box-5");
+const inputBoxEl1 = document.getElementById("input-el-1");
+const inputBoxEl2 = document.getElementById("input-el-2");
+const inputBoxEl3 = document.getElementById("input-el-3");
+const inputBoxEl4 = document.getElementById("input-el-4");
+const inputBoxEl5 = document.getElementById("input-el-5");
 
 // preparo variabile array numeri generati
 let numbersGenerated = [];
+
+// preparo variabile arrau numeri inseriti dall'utente
+let userInputNumbers = [];
 
 // parametro max per "generateRandomNumbers"
 const numMax = 99;
@@ -31,27 +35,53 @@ const stampRandomNums = () => {
   for (let i = 0; i < 5; i++) {
     // genero 5 numeri random
     const genNum = generateRandomNumbers(numMax);
-    // inserisco numeri generati nell'array
+    // inserisco numeri generati nell'array "numbersGenerated"
     numbersGenerated.push(genNum);
   }
 
   // stampo in pagina i numeri
-  gameRandomNums.innerText = numbersGenerated.join(" || ");
+  gameRandomNumsBox.innerText = numbersGenerated.join(" || ");
 };
 
 // funzione che fa iniziare il gioco
 const gameStart = () => {
-  gameRandomNums.innerText = "";
-  gameInput.classList.remove("d-none");
+  gameRandomNumsBox.innerText = "";
+  gameInputBox.classList.remove("d-none");
 };
 
-// aggiungere una funzione che raccoglie i valori immessi dall'utente
+// funzione che raccoglie i numeri inseriti dall'utente
+const userNumbersValue = () => {
+  // valori in numero inseriti dall'utente
+  const valueInput1 = parseInt(inputBoxEl1.value);
+  const valueInput2 = parseInt(inputBoxEl2.value);
+  const valueInput3 = parseInt(inputBoxEl3.value);
+  const valueInput4 = parseInt(inputBoxEl4.value);
+  const valueInput5 = parseInt(inputBoxEl5.value);
+
+  // li inserisco nell'array "userInputNumbers"
+  userInputNumbers.push(valueInput1);
+  userInputNumbers.push(valueInput2);
+  userInputNumbers.push(valueInput3);
+  userInputNumbers.push(valueInput4);
+  userInputNumbers.push(valueInput5);
+  console.log(userInputNumbers);
+};
+
+// # OUTPUT
+
+// aggiungo funzione per la fine del gioco
+gameInputBox.addEventListener("submit", (e) => {
+  // blocco l'evento submit
+  e.preventDefault();
+  // recupero i numeri inseriti dall'utente
+  userNumbersValue();
+});
 
 // stampo i numeri da memorizzare in pagina
 stampRandomNums();
 
 // faccio partire il gioco dopo un timer
-const startGame = setTimeout(gameStart, 5000);
+const startGame = setTimeout(gameStart, 1000);
 
 // | DEBUG CONSOLE LOG
 console.log(numbersGenerated);
